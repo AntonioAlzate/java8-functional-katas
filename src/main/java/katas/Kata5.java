b3ee36ae8eec533b7ac2564d748c024a1176e835
Antonio Alzate <antonioalzate7@gmail.com>
2021-12-09 12:26:14 -0500

    Completada Kata 1,2 y 5

--- src/main/java/katas/Kata5.java
+++ src/main/java/katas/Kata5.java
@@ -19,6 +19,8 @@ public class Kata5 {
     public static Double execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return 3.0;
+        return movies.stream()
+                .reduce((acumular, rating) -> acumular.getRating() > rating.getRating() ? acumular : rating).get().getRating();
+
     }
 }
